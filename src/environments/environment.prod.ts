086c0c65d009ec3fd5b82abcd6ce6f39690c15fa
Geanik <Geanik@users.noreply.github.com>
2025-02-16 17:51:22 +0100

    Add separate env and configuration for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: false,
     spotify: {
         clientId: 'baeba4741ddf4982ba14d5f4b4735894',
-        redirectUri: 'http://localhost:4200',
+        redirectUri: 'https://audiobook-player.netlify.app',
     },
 };
